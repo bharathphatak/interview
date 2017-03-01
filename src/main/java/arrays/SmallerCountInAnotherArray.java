bc8714bd2e4d2c608f3335fb1e7cdbe00d282679
bharath <bharath.phatak@gmail.com>
2017-03-01 20:22:50 +0530

    SmallestCountInAnotherArray

---
+++ src/main/java/arrays/SmallerCountInAnotherArray.java
@@ -0,0 +1,34 @@
+package arrays;
+
+import sun.awt.image.ImageWatched;
+
+import java.util.*;
+
+/**
+ * Created by bhara on 01-03-2017.
+ */
+public class SmallerCountInAnotherArray {
+    /**
+     * Given two array, for each element in array A,find number of elements smaller or equal in another array b
+     */
+
+    public static void main(String[] args){
+
+        Integer a[] = {10,4,7,9,13};
+        Integer b[] = {6,9,16,4,1};
+
+        List<Integer> A = Arrays.asList(a);
+        List<Integer> B = Arrays.asList(a);
+
+        Collections.sort(A);
+
+        Collections.sort(B);
+
+
+
+
+
+    }
+
+
+}
